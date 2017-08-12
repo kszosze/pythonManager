1dcb26e69a2074bae1ea84db61d72849c59aefbb
jose garcia <kszosze@gmail.com>
2017-08-12 12:10:25 +0100

    Adding log and error messages

--- processmanager.py
+++ processmanager.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import logging
 import json
 import subprocess
 import threading
@@ -10,6 +11,7 @@ def getProcessName(process):
 
 def execute(process):
     process = None
+    logger.info("Runing app " + getProcessName(process))
     if 'cwd' in process:
         process = subprocess.Popen(process["command"], cwd=process["cwd"])
         while process.returncode is None:
@@ -19,23 +21,37 @@ def execute(process):
         while process.returncode is None:
               process = subprocess.Popen(process["command"])
     process_map[getProcessName(process)]=process
+    logger.info("App " + getProcessName(process) + " is running")
 
 def stop(process):
+    logger.info("Stopping app " + getProcessName(process))
     process.terminate()
     while process.returncode is not None:
         process.kill()
+    logger.info("App " + getProcessName(process) + " stopped")
+
+def error(self, reason):
+        d = json.dumps({ "status" : "error", "reason" : reason})
+        return app.HTTPError("400 Bad Request", {"Content-type": "application/json"}, d)
 
 class processFilter:
     def POST(self):
+        logger.info("Message with commands has been received")
+        web.header('Content-Type', 'application/json')
+        if not web.data():
+            log.error("Empty String received")
+            error(self, "Wrong content")
+
         processList = json.loads(web.data())
         for process in processList:
             if (process["state"]=="running"):
                 d=threading.Thread(name='daemon', target=execute(process))
-                d.setDaemon(True)
+                d.setDaemon(False)
             elif (process["state"]=="stopped"):
                 d=threading.Thread(name='daemon', target=stop(process_map[getProcessName(process)]))
-                d.setDaemon(True)
-
+                d.setDaemon(False)
+            else:
+                error(self, "Wrong data")
 urls = (
     '/process', 'processFilter',
 )
